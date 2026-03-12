eb30ca5a6d3ce243155fd38c376c8bfffe0ddfd9
dcchill <dariancsmart@gmail.com>
2026-03-12 08:27:43 -0400

    MCreator Merge commit 'ff2daa11a9d8fd78e4140ce01ba89455b5cad849' into 5540ab8b9e18a289ab0c877dfad518c347cde9d8

--- src/main/java/net/create_nomad/CreateNomadMod.java
+++ src/main/java/net/create_nomad/CreateNomadMod.java
@@ -29,7 +29,6 @@ import net.create_nomad.init.CreateNomadModTabs;
 import net.create_nomad.init.CreateNomadModSounds;
 import net.create_nomad.init.CreateNomadModMenus;
 import net.create_nomad.init.CreateNomadModItems;
-import net.create_nomad.init.CreateNomadModDyedBackpackHooks;
 import net.create_nomad.init.CreateNomadModCuriosRenderers;
 import net.create_nomad.init.CreateNomadModCuriosCompat;
 import net.create_nomad.init.CreateNomadModBlocks;
@@ -60,10 +59,12 @@ public class CreateNomadMod {
 		CreateNomadModBlocks.REGISTRY.register(modEventBus);
 		CreateNomadModBlockEntities.REGISTRY.register(modEventBus);
 		CreateNomadModItems.REGISTRY.register(modEventBus);
+
 		CreateNomadModTabs.REGISTRY.register(modEventBus);
+
 		CreateNomadModMenus.REGISTRY.register(modEventBus);
+
 		// Start of user code block mod init
-		CreateNomadModDyedBackpackHooks.register(modEventBus);
 		// End of user code block mod init
 	}
 
