e8aa9e3118d3b4781962541d36f262804889be35
dcchill <daria@DESKTOP-PAJVH85>
2026-03-18 13:32:56 -0400

    weird crash

--- src/main/java/net/create_nomad/init/CreateNomadModAdditionalModels.java
+++ src/main/java/net/create_nomad/init/CreateNomadModAdditionalModels.java
@@ -6,11 +6,12 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ModelEvent;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 
 @EventBusSubscriber(modid = CreateNomadMod.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class CreateNomadModAdditionalModels {
-	@SubscribeEvent
-	public static void registerAdditionalModels(ModelEvent.RegisterAdditional event) {
-		event.register(SanderItemRenderer.ACCELERATOR_MODEL_ID);
-	}
+		@SubscribeEvent
+		public static void registerAdditionalModels(ModelEvent.RegisterAdditional event) {
+		    event.register(new ModelResourceLocation(SanderItemRenderer.ACCELERATOR_MODEL_ID, "inventory"));
+		}
 }
