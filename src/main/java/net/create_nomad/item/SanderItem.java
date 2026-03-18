358f3d7e022605246042294b86709e8e24eae895
dcchill <136468998+dcchill@users.noreply.github.com>
2026-03-18 13:29:46 -0400

    Fix sander accelerator model loading

--- src/main/java/net/create_nomad/item/SanderItem.java
+++ src/main/java/net/create_nomad/item/SanderItem.java
@@ -1,9 +1,21 @@
 package net.create_nomad.item;
 
-import net.minecraft.world.item.Item;
+import java.util.function.Consumer;
 
-public class SanderItem extends Item {
+import com.simibubi.create.content.equipment.sandPaper.SandPaperItem;
+import com.simibubi.create.foundation.item.render.SimpleCustomRenderer;
+
+import net.create_nomad.item.renderer.SanderItemRenderer;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+
+public class SanderItem extends SandPaperItem {
 	public SanderItem() {
 		super(new Item.Properties().stacksTo(1));
+	}
+
+	@Override
+	public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+		consumer.accept(SimpleCustomRenderer.create(this, new SanderItemRenderer()));
 	}
 }
