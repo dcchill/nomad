bd006674e7d39b2280c8d96839c21455dad577ee
dcchill <dariancsmart@gmail.com>
2026-03-23 11:10:38 -0400

    toolbelt

---
+++ src/main/java/net/create_nomad/item/ToolbeltItem.java
@@ -0,0 +1,9 @@
+package net.create_nomad.item;
+
+import net.minecraft.world.item.Item;
+
+public class ToolbeltItem extends Item {
+	public ToolbeltItem() {
+		super(new Item.Properties().stacksTo(1));
+	}
+}
